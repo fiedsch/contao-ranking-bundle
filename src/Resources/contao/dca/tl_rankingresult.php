1063779a8a39d5062cdb951252636828e907cdf0
Andreas Fieger <fiedsch@ja-eh.at>
2026-01-31 13:25:35 +0100

    Handle undefined index
    occurs when running migrations

--- src/Resources/contao/dca/tl_rankingresult.php
+++ src/Resources/contao/dca/tl_rankingresult.php
@@ -106,7 +106,7 @@ $GLOBALS['TL_DCA']['tl_rankingresult'] = [
                 'label' => &$GLOBALS['TL_LANG']['tl_rankingresult']['delete'],
                 'href' => 'act=delete',
                 'icon' => 'delete.svg',
-                'attributes' => 'onclick="if (!confirm(\''.$GLOBALS['TL_LANG']['MSC']['deleteConfirm'].'\')) return false; Backend.getScrollOffset();"',
+                'attributes' => 'onclick="if (!confirm(\''.($GLOBALS['TL_LANG']['MSC']['deleteConfirm'] ?? '').'\')) return false; Backend.getScrollOffset();"',
             ],
 
             'show' => [
