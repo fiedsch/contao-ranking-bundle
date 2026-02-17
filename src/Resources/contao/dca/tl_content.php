d43726fdc040df4e53dcbf5676a81034631ddef8
Andreas Fieger <fiedsch@ja-eh.at>
2026-02-17 10:15:36 +0100

    Add missing DCA configuration for tl_content

---
+++ src/Resources/contao/dca/tl_content.php
@@ -0,0 +1,3 @@
+<?php
+
+$GLOBALS['TL_DCA']['tl_content']['palettes']['rankingranking'] = '{type_legend},type,headline,title;{template_legend:hide},customTpl;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID,space;{invisible_legend:hide},invisible,start,stop';
